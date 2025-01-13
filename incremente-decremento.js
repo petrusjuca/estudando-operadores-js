f3438924f6da284f8952eae5df039d8c4424cdae
petrusjuca <146126655+petrusjuca@users.noreply.github.com>
2025-01-13 20:58:27 -0300

    Create incremente-decremento.js
    Este arquivo explora os operadores de incremento (++) e decremento (--) em JavaScript. São apresentados exemplos práticos que demonstram como esses operadores podem ser utilizados para aumentar ou diminuir o valor de uma variável de forma simples e eficiente. O conteúdo inclui a explicação das diferenças entre as versões prefixada e posfixada dos operadores, além de exemplos que ilustram seu comportamento em expressões e loops. O código está comentado para facilitar o entendimento, sendo ideal para quem deseja aprimorar o uso desses operadores no contexto de manipulação de variáveis e controle de fluxo.

---
+++ incremente-decremento.js
@@ -0,0 +1,13 @@
+let contador = 1
+// ++ incremento
+contador++ 
+// contador = contador + 1 // mesma coisa fi
+// -- decremento
+console.log(contador)
+
+// '--' decremento
+contador--
+contador--
+contador--
+
+console.log(contador)

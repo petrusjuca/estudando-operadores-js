a66cdfd3df3e35cb1e3717603d265c9c754322ba
petrusjuca <146126655+petrusjuca@users.noreply.github.com>
2025-01-13 21:08:05 -0300

    Create operadores-atribuicao.js

---
+++ operadores-atribuicao.js
@@ -0,0 +1,11 @@
+let preco = 10
+preco = preco + 4
+
+/// vc pode tbm fzr isso de uma maneira melhorm vc pode atribuir e somar de uma vez
+
+// se vc coloca um sinal matematico antes de um de atribuicao ele atribui a operacao
+// aritmetica a variavel, pode ser somando, subtraindo, multiplicando, ppd ser com tudo
+console.log(preco);
+
+
+// quero incrementar um valor nessa variavel
